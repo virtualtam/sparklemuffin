b5b89d1b1f6e110628c48df0139088317d194328
VirtualTam <virtualtam@flibidi.net>
2022-03-26 22:05:07 +0100

    srv: setup structured logging with Zerolog
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- cmd/yawbe-srv/main.go
+++ cmd/yawbe-srv/main.go
@@ -2,31 +2,56 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
+	"github.com/rs/zerolog/log"
 	"github.com/virtualtam/yawbe/pkg/http/www"
 )
 
 const (
-	defaultListenAddr = ":8080"
+	defaultDebugMode  = false
+	defaultListenAddr = "0.0.0.0:8080"
 )
 
 func main() {
+	debugMode := flag.Bool("debug", defaultDebugMode, "Enable debugging")
 	listenAddr := flag.String("listenAddr", defaultListenAddr, "Listen on this address")
 	flag.Parse()
+
+	if *debugMode {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
 
 	router := mux.NewRouter()
 
 	www.AddRoutes(router)
 
-	srv := &http.Server{
+	// Structured loging
+	chain := alice.New(hlog.NewHandler(log.Logger), hlog.AccessHandler(accessLogger))
+
+	server := &http.Server{
 		Addr:         *listenAddr,
-		Handler:      router,
+		Handler:      chain.Then(router),
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
-	log.Fatal(srv.ListenAndServe())
+	log.Info().Msgf("Listening to http://%s", *listenAddr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal().Err(err).Msg("ListenAndServe")
+	}
+}
+
+func accessLogger(r *http.Request, status, size int, dur time.Duration) {
+	hlog.FromRequest(r).Info().
+		Dur("duration_ms", dur).
+		Str("host", r.Host).
+		Str("path", r.URL.Path).
+		Int("size", size).
+		Int("status", status).
+		Msg("Request")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/virtualtam/yawbe
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/justinas/alice v1.2.0
+	github.com/rs/zerolog v1.26.1
+)
+
+require github.com/rs/xid v1.3.0 // indirect

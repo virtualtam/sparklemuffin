19469ee798f4b7ed6f17447aa0b24a08e0cc5c0d
VirtualTam <virtualtam@flibidi.net>
2022-03-26 11:07:33 +0100

    www: update comments
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- pkg/http/www/routes.go
+++ pkg/http/www/routes.go
@@ -7,15 +7,17 @@ import (
 	"github.com/virtualtam/yawbe/pkg/http/www/static"
 )
 
+// AddRoutes registers all HTTP handlers for the Web interface.
 func AddRoutes(r *mux.Router) {
 	r.Handle("/", HomeView)
-	r.HandleFunc("/static/", http.NotFound)
 
+	r.HandleFunc("/static/", http.NotFound)
 	r.PathPrefix("/static/").Handler(http.StripPrefix(
 		"/static/",
 		cacheControlWrapper(http.FileServer(http.FS(static.FS)))))
 }
 
+// cacheControlWrapper sets the Cache-Control header.
 func cacheControlWrapper(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "max-age=2592000") // 30 days

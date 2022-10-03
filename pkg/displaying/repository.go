09a5a019afd160c82f2aefed035ec86258a8f645
VirtualTam <virtualtam@flibidi.net>
2022-10-03 19:50:09 +0200

    displaying: update repository documentation
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- pkg/displaying/repository.go
+++ pkg/displaying/repository.go
@@ -2,10 +2,12 @@ package displaying
 
 import "github.com/virtualtam/yawbe/pkg/bookmark"
 
+// Repository provides access to query user bookmarks.
 type Repository interface {
-	// BookmarkGetCount returns the number of bookmarks for a  given user.
+	// BookmarkGetCount returns the number of bookmarks for a given user.
 	BookmarkGetCount(userUUID string) (int, error)
 
 	// BookmarkGetN returns at most n bookmarks for a given user, starting at
+	// a given offset.
 	BookmarkGetN(userUUID string, n int, offset int) ([]bookmark.Bookmark, error)
 }

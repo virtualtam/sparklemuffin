42283accce10e1cc5fffff8f59b8844c2bd5049f
VirtualTam <virtualtam@flibidi.net>
2023-06-07 00:17:36 +0200

    hash: add sync.Locker to HMAC Reset/Write/Sum operations
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- pkg/hash/hmac.go
+++ pkg/hash/hmac.go
@@ -7,15 +7,20 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"hash"
+	"sync"
 )
 
 // HMAC provides helpers to generate strong application tokens.
 type HMAC struct {
 	hmac hash.Hash
+	l    sync.Locker
 }
 
 // Hash returns the Base64 URL-encoded HMAC hash of the provided input.
 func (h *HMAC) Hash(input string) (string, error) {
+	h.l.Lock()
+	defer h.l.Unlock()
+
 	h.hmac.Reset()
 
 	_, err := h.hmac.Write([]byte(input))
@@ -32,5 +37,6 @@ func NewHMAC(key string) *HMAC {
 	h := hmac.New(sha256.New, []byte(key))
 	return &HMAC{
 		hmac: h,
+		l:    &sync.Mutex{},
 	}
 }

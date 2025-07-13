8c5cbd6d9323a51a95c33f8bd573f4310aa9e7b7
VirtualTam <virtualtam@flibidi.net>
2025-07-13 23:27:05 +0200

    errors: prefix all messages with the domain name
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- pkg/session/errors.go
+++ pkg/session/errors.go
@@ -6,7 +6,7 @@ package session
 import "errors"
 
 var (
-	ErrNotFound                  = errors.New("not found")
-	ErrRememberTokenRequired     = errors.New("remember token required")
-	ErrRememberTokenHashRequired = errors.New("remember token hash required")
+	ErrNotFound                  = errors.New("session: not found")
+	ErrRememberTokenRequired     = errors.New("session: remember token required")
+	ErrRememberTokenHashRequired = errors.New("session: remember token hash required")
 )

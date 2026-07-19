6dd61788e8a2b2858284405f442e618d1349b2f2
VirtualTam <virtualtam@flibidi.net>
2026-07-19 03:09:02 +0200

    ci: delint SQL migration
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- internal/repository/postgresql/migrations/000011_alter_feed_feeds_table_add_unique_slug.up.sql
+++ internal/repository/postgresql/migrations/000011_alter_feed_feeds_table_add_unique_slug.up.sql
@@ -5,4 +5,4 @@
 -- uniqueness before enforcing the constraint.
 UPDATE feed_feeds SET slug = slug || '-' || LEFT(uuid::text, 8);
 
-ALTER TABLE feed_feeds ADD CONSTRAINT feed_feeds_slug_unique UNIQUE (slug);
+ALTER TABLE feed_feeds ADD CONSTRAINT feed_feeds_slug_unique UNIQUE(slug);
